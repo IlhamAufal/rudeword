dc392fba85971f8a87ab64a174031f82390cfe1f
IlhamAufal <aufalilham@gmail.com>
2024-12-16 07:18:52 +0700

    Done Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,31 @@ from translate import Translator
 import re
 import dill
 
-clean_data = dill.load(open('clean_data.sav', 'rb'))
-normalize = dill.load(open('normalize.sav', 'rb'))
-stemming = dill.load(open('stemming.sav', 'rb'))
-translate_id = dill.load(open('translate_id.sav', 'rb'))
-replace_kata_kasar = dill.load(open('replace_kata_kasar.sav', 'rb'))
-execute_function = dill.load(open('execute_function.sav', 'rb'))
+clean_data = dill.load(open('saveFile/clean_data.sav', 'rb'))
+normalize = dill.load(open('saveFile/normalize.sav', 'rb'))
+stemming = dill.load(open('saveFile/stemming.sav', 'rb'))
+translate_id = dill.load(open('saveFile/translate_id.sav', 'rb'))
+replace_kata_kasar = dill.load(open('saveFile/replace_kata_kasar.sav', 'rb'))
+execute_function = dill.load(open('saveFile/execute_function.sav', 'rb'))
 
 # Streamlit interface
 st.title("Rude Word Replacement")
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1, 1])
 
 with col1:
-    footage = st.text_input('Input paragraph')
+    st.subheader('Input Paragraph')
+    footage = st.text_input('')
+    result=''
+    if st.button('Apply'):
+        footage = clean_data(footage)
+        footage = normalize(footage)
+        footage = stemming(footage)
+        footage = translate_id(footage)
+        result = replace_kata_kasar(footage)
+        # result = execute(footage)
+        # st.success(result)    
 
 with col2:
-    if st.button('Apply'):
-            footage = clean_data(footage)
-            footage = normalize(footage)
-            footage = stemming(footage)
-            footage = translate_id(footage)
-            result = replace_kata_kasar(footage)
-            # result = execute(footage)
-            st.success(result)
+     st.subheader('Result')
+     st.write(result)
