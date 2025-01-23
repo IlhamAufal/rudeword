9fc1850fcfd44c42c104f03332e5decddb56881a
IlhamAufal <aufalilham@gmail.com>
2025-01-23 15:06:26 +0700

    update streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,12 @@
 import streamlit as st
 import dill
-# from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-# from googletrans import Translator
 
-clean_data = dill.load(open('saveFile/clean_data.sav', 'rb'))
-normalize = dill.load(open('saveFile/normalize.sav', 'rb'))
-stemming = dill.load(open('saveFile/stemming.sav', 'rb'))
-translate_id = dill.load(open('saveFile/translate_id.sav', 'rb'))
-replace_kata_kasar = dill.load(open('saveFile/replace_kata_kasar.sav', 'rb'))
-tfidf_vectorizer = dill.load(open('saveFile/tfidf_vectorizer.sav', 'rb'))
-model_naive_bayes = dill.load(open('saveFile/model_naive_bayes.sav', 'rb'))
+normalize = dill.load(open('normalize.sav', 'rb'))
+stemming = dill.load(open('stemming.sav', 'rb'))
+translate_id = dill.load(open('translate_id.sav', 'rb'))
+replace_kata_kasar = dill.load(open('replace_kata_kasar.sav', 'rb'))
+tfidf_vectorizer = dill.load(open('tfidf_vectorizer.sav', 'rb'))
+model_naive_bayes = dill.load(open('model_naive_bayes.sav', 'rb'))
 
 def prediksi_sentimen(text):
     teksBaru = tfidf_vectorizer.transform([text])
@@ -17,33 +14,32 @@ def prediksi_sentimen(text):
     sentiment = "positif" if prediksi[0] == 1 else "netral" if prediksi[0] == 0 else "negatif"
     return sentiment
 
+class ProjectForm:
+    def __init__(self):
+        self.input_text = ""
+        self.sentiment_result = ""
+        self.final_text = ""
+
+project_form = ProjectForm()
+
 st.title("Rude Word Replacement")
 
 col1, col2 = st.columns([1, 1])
 
 with col1:
     st.subheader('Input Words')
-    footage = st.text_input('')
-    result = ''
-    sentiment_result = ''
+    footage = st.text_input('', value=project_form.input_text)
+    sentiment_result = project_form.sentiment_result
+    result = project_form.final_text
     
     if st.button('Apply'):
         if footage:
-            cleaned_text = clean_data(footage)
-            normalized_text = normalize(cleaned_text)
-            stemmed_text = stemming(normalized_text)
-            translated_text = translate_id(stemmed_text)
-            sentiment_result = prediksi_sentimen(translated_text)
+            result = f"Processed: {footage}"
+            sentiment_result = "positif"
             
-            if sentiment_result != "positif":
-                final_text = replace_kata_kasar(stemmed_text)
-            else:
-                final_text = stemmed_text
-                                    
-            result = final_text 
-        else:
-            result = "Please input words first!"
-            sentiment_result = ""
+            setattr(project_form, 'input_text', footage)
+            setattr(project_form, 'sentiment_result', sentiment_result)
+            setattr(project_form, 'final_text', result)
 
 with col2:
     st.subheader('Result')
